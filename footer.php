a3f3498b7b2676685ee6062dd42cd975480ca365
lsul2032-sys <lsul2032@chadwickschool.org>
2026-03-08 08:35:54 +0900

    Update footer to include new developer names

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
     <div class="wicky-mascot"><i class="fas fa-water"></i></div>
     <p>Chadwick International - Grade 6 Sixers</p>
     <p>© 2026 Sixers Website Club</p>
-    <p> Website Programmed by Taeyang Eum, Shane Choi</p>
+    <p> Frontend Devs: Taeyang Eum, Shane Choi, Daniel Kim</p>
+    <p> Backend: Lucas Sul </p>
     <p><a href="https://www.youtube.com/@CI_sixers/videos" target="_blank" style="color: white; text-decoration: none;"><i class="fab fa-youtube"></i> CI Sixers YouTube</a></p>
   </footer>

aa2d532e553d4fcc191ac3b3375df835339b8446
lsul2032-sys <lsul2032@chadwickschool.org>
2026-03-08 16:29:44 +0900

    testing organization

---
+++ organize.js
@@ -0,0 +1,11 @@
+
+fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById("footer").innerHTML = data;
+});
+fetch("nav.html")
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById("nav").innerHTML = data;
+});

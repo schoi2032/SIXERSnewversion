d80cb6d0fe6662df62e64c9935a8e2fc41f726f7
lsul2032-sys <lsul2032@chadwickschool.org>
2026-03-14 21:47:43 +0900

    PLEASE CHANGE THE VERSION NUMBER ON FOOTER.HTML FOR EVERY COMMIT YOU MAKE. THIS IS TO TRACK THE PROGRESS OF THE WEBSITE AND TO AVOID CONFLICTS IN THE FUTURE. THANK YOU!

--- script.js
+++ script.js
@@ -174,3 +174,45 @@ function initComedySlideshow() {
 
   showSlide(currentIndex);
 }
+
+
+fetch("../footer.html")
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById("footer").innerHTML = data;
+});
+fetch("../nav.html")
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById("nav").innerHTML = data;
+    switch (type) {
+    case "0":
+        document.querySelector("#nav a[href='index.html']").classList.add("active");
+        break;
+    case "1":
+        document.querySelector("#nav a[href='news.html']").classList.add("active");
+        break;
+    case "2":
+        document.querySelector("#nav a[href='comedy.html']").classList.add("active");
+        break;
+    case "3":
+        document.querySelector("#nav a[href='fun-facts.html']").classList.add("active");
+        break;
+    case "4":
+        document.querySelector("#nav a[href='credits.html']").classList.add("active");
+        break;
+    case "5":
+        document.querySelector("#nav a[href='spotlight.html']").classList.add("active");
+        break;
+    case "6":
+        document.querySelector("#nav a[href='about.html']").classList.add("active");
+        break;
+    case "7":
+        document.querySelector("#nav a[href='Boom-trip-photos.html']").classList.add("active");
+        break;  
+}
+});
+
+
+
+
